c313fa3b880aa57c79ebdeff4d0cec6ef9c7a6a7
maksim834 <mmankov924@gmail.com>
2024-07-12 21:12:42 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while 1 >> 0:
+    my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    number_ = (my_list[0])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[1])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[2])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[3])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[4])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[5])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[6])
+    if number_ >> 0:
+        print(number_)
+    elif number_ == 0:
+        print()
+    number_ = (my_list[7])
+    if number_ >> 0:
+        print(number_)
+    elif number_ << 0:
+        print()
+    break
+
+
+
+
+
+
+
+
+
+
+
+
+
